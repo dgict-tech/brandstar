0764e1c5771a80904715d5ae6664ea9cd6421cff
Dan George <dangeorge35@gmail.com>
2025-11-19 07:01:55 +0100

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // <— THIS replaces next export
 };
 
 export default nextConfig;
